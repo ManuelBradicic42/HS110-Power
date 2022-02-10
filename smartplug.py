24b92baf81f06232ca806b2d873f041d8639039e
Manuel Bradičić <36088783+ManuelBradicic42@users.noreply.github.com>
2022-02-10 14:03:45 +0000

    Add files via upload

---
+++ smartplug.py
@@ -0,0 +1,30 @@
+import sys
+import keyboard
+from pyHS100 import SmartPlug
+from pprint import pformat as pf 
+from datetime import datetime
+from csv import writer
+
+ip = sys.argv[1]
+plug = SmartPlug(ip)
+file = open("powerlog.csv", "w")
+print("Harware: %s" % pf(plug.hw_info))
+
+while True:
+	try:
+		if keyboard.is_pressed('q'):
+			print('You pressed the key')
+			break
+		else:
+			activePower = plug.current_consumption()
+			currentTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			list_data = [currentTime,activePower]
+			print(list_data)
+			with open('powerlog.csv', 'a', newline='') as f_object:
+				writer_object = writer(f_object)
+				writer_object.writerow(list_data)
+				f_object.close()
+	except Exception as e:
+		print(e.message, e.args)
+		break
+		
